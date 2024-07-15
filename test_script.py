e88ddc737f959a5fa3af5378341007c385083c55
Jiri Nemecek <contact@nemecekjiri.cz>
2024-07-15 19:52:48 +0200

    Introduce argparse

--- test_script.py
+++ test_script.py
@@ -1,12 +1,27 @@
-import sys
+import argparse
 
 import numpy as np
 
 from methods import test_BRCG, test_one_rule, test_RIPPER
-from scenarios.MEPS_scenarios import load_scenario
+from scenarios.MEPS_scenarios import SCENARIOS as MEPS_SCENARIOS
+from scenarios.MEPS_scenarios import load_scenario as load_MEPS_scenario
 from utils import accuracy, balance_datasets, our_metric
 
-binarizer, input_data, target_data = load_scenario("stress")
+SCENARIOS = MEPS_SCENARIOS  # prepared for combining with other sources of scenarios
+
+parser = argparse.ArgumentParser(
+    prog="DNF for bias",
+    description="Benchmark search for bias in data using DNF learning",
+    epilog="Research work, subject to change",
+)
+parser.add_argument("-s", "--scenario", required=True, choices=SCENARIOS)
+parser.add_argument("--brcg", action="store_true")
+parser.add_argument("--ripper", action="store_true")
+parser.add_argument("--onerule", action="store_true")
+args = parser.parse_args()
+
+if args.scenario in MEPS_SCENARIOS:
+    binarizer, input_data, target_data = load_MEPS_scenario(args.scenario)
 
 X_bin = binarizer.encode(input_data)
 X_bin_neg = binarizer.encode(input_data, include_negations=True)
@@ -23,9 +38,9 @@ else:
 
 y_bin, X_bin, X_bin_neg = balance_datasets(y_bin, [y_bin, X_bin, X_bin_neg], seed=seed)
 n, d = X_bin.shape
-print(f"balancing dropped {n_orig-n} samples, {n} remain. \nDimension is {d}.\n")
+print(f"Balancing dropped {n_orig-n} samples, {n} remain. \nDimension is {d}.\n")
 
-if sys.argv[1] == "RIPPER":
+if args.ripper:
     y_est, rules = test_RIPPER(
         X_bin,
         y_bin,
@@ -33,7 +48,7 @@ if sys.argv[1] == "RIPPER":
         binarizer,
         verbose=True,
     )
-elif sys.argv[1] == "BRCG":
+if args.brcg:
     y_est, rules = test_BRCG(
         X_bin_neg,
         y_bin,
@@ -46,7 +61,7 @@ elif sys.argv[1] == "BRCG":
             "solver": "CLARABEL",
         },
     )
-elif sys.argv[1] == "onerule":
+if args.onerule:
     y_est, rules = test_one_rule(
         X_bin_neg,
         y_bin,
