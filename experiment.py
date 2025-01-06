99d0b463a84afbdc2a7a097b0727843d332c5e32
Jiri Nemecek <contact@nemecekjiri.cz>
2025-01-06 10:29:50 +0100

    Add git checks and version info to experiments

--- experiment.py
+++ experiment.py
@@ -1,11 +1,14 @@
 # python experiment.py -m
 
+import os
+import subprocess
+
 import hydra
 from omegaconf import DictConfig
-import subprocess
-import os
 
-@hydra.main(version_base=None, config_path="conf", config_name="config")
+githash = ""
+
+@hydra.main(version_base="1.3", config_path="conf", config_name="config")
 def run_experiment(cfg: DictConfig):
     command = ["python", "test_script.py"]
 
@@ -18,23 +21,24 @@ def run_experiment(cfg: DictConfig):
     if cfg.seed is not None:
         command.extend(["--seed", str(cfg.seed)])
     if cfg.rho is not None:
-        command.extend([str(cfg.rho)])
+        command.extend(["--rho", str(cfg.rho)])
     if cfg.method is not None:
         command.extend(["-m", str(cfg.method)])
+    if cfg.method is not None:
+        command.extend(["-nm", str(cfg.n_min)])
     if cfg.k is not None:
         command.extend(["-k", str(cfg.k)])
-    else:
-        command.extend(["-k", str(cfg.dimension)])
 
     command.extend(["--verbose"])
 
     print("Running command:", " ".join(command))
-    
+
     # Run the command and capture the output
     try:
         result = subprocess.run(command, capture_output=True, text=True, timeout=310)
     except subprocess.TimeoutExpired as exc:
         print("Worked too long. Process finished without result.")
+        result = None
 
     # Get the current working directory, which Hydra sets for each run
     run_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
@@ -42,10 +46,19 @@ def run_experiment(cfg: DictConfig):
     # Save the output and error logs to a file in the current run directory
     with open(os.path.join(run_dir, "output.txt"), "w") as out_file:
         out_file.write("Command:\n" + " ".join(command) + "\n")
-        out_file.write("Output:\n" + result.stdout + "\n")
-        out_file.write("Errors:\n" + result.stderr + "\n")
+        out_file.write(f"Git hash: {githash}\n")
+        if result is not None:
+            out_file.write("Output:\n" + result.stdout + "\n")
+            out_file.write("Errors:\n" + result.stderr + "\n")
 
     print(f"Result saved to {os.path.join(run_dir, 'output.txt')}")
 
+
 if __name__ == "__main__":
-    run_experiment()
+    result = subprocess.run(["git", "status", "--porcelain"], capture_output=True, text=True)
+    if result.stdout.strip() == "":
+        res = subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True, text=True)
+        githash = res.stdout.strip()
+        run_experiment()
+    else:
+        raise Exception("Git status is not clean. Commit changes first.")
