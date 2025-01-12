7d75dcb504b9f30284f6513e6914d31c7a7b48dd
Jiri Nemecek <contact@nemecekjiri.cz>
2025-01-12 21:58:48 +0100

    Extend plotting scripts for real data comparison

--- plot-maker/real_data_line.py
+++ plot-maker/real_data_line.py
@@ -59,73 +59,96 @@ def extract_data_for_method(method):
                         spsf_str = re.search(r"SPSF violation:\s*([0-9.]+)", line)
                         if spsf_str:
                             extracted_data.append(
-                                ("SPSF violation", scenario, float(spsf_str.group(1)))
-                            )
-                        our_str = re.search(r"Our objective:\s*([0-9.]+)", line)
-                        if our_str:
-                            extracted_data.append(
-                                ("Our Objective", scenario, float(our_str.group(1)))
+                                (
+                                    "SPSF violation",
+                                    scenario,
+                                    n_samples,
+                                    float(spsf_str.group(1)),
+                                )
                             )
                         time_str = re.search(r"Seconds needed:\s*([0-9.]+)", line)
                         if time_str:
                             extracted_data.append(
-                                ("Time [s]", scenario, float(time_str.group(1)))
+                                (
+                                    "Time [s]",
+                                    scenario,
+                                    n_samples,
+                                    float(time_str.group(1)),
+                                )
                             )
-                    extracted_data.append(("# of samples", scenario, n_samples))
 
     data_dict = {}
 
     for (
         valname,
         scenario,
+        n_samples,
         val,
     ) in extracted_data:
         if valname not in data_dict:
-            data_dict[valname] = defaultdict(list)
-        data_dict[valname][scenario].append(val)
+            data_dict[valname] = {}
+        if scenario not in data_dict[valname]:
+            data_dict[valname][scenario] = defaultdict(list)
+        data_dict[valname][scenario][n_samples].append(val)
 
     return data_dict
 
 
 all_data = {}
 for method in methods:
-    print(method)
     all_data[method] = extract_data_for_method(method)
 
-r, c = 2, 2
-fig, axs = plt.subplots(r, c, figsize=(12, 8))
-
-names = sorted(list(all_data[methods[0]].values())[0].keys())
+r, c = 10, 2
+fig, axs = plt.subplots(r, c, figsize=(12, 30))
 
 for method in methods:
     data_dict = all_data[method]
     for i, measure in enumerate(sorted(data_dict.keys())):
-        ax = axs[i // c, i % c]
+        for j, scenario in enumerate(sorted(data_dict[measure].keys())):
+            ax = axs[j, i]
 
-        sorted_mean = [np.mean(data_dict[measure][n]) for n in names]
-        sorted_std = [np.std(data_dict[measure][n]) for n in names]
+            sorted_n_samples = sorted(data_dict[measure][scenario].keys())
+            sorted_mean = [
+                np.mean(data_dict[measure][scenario][n]) for n in sorted_n_samples
+            ]
+            sorted_std = [
+                np.std(data_dict[measure][scenario][n]) for n in sorted_n_samples
+            ]
 
-        barwidth = 0.35
-        shift = (barwidth / 2) if method == methods[0] else (-barwidth / 2)
-        # barplot with std band for each method side by side in one axes
-        ax.bar(
-            np.arange(len(names)) + shift,
-            sorted_mean,
-            barwidth,
-            yerr=sorted_std,
-            capsize=5,
-            color=method_colors[method],
-            label=f"{method_names[method]} + std band",
-        )
+            ax.fill_between(
+                sorted_n_samples,
+                np.array(sorted_mean) - np.array(sorted_std),
+                np.array(sorted_mean) + np.array(sorted_std),
+                color=method_colors[method],
+                alpha=0.2,
+            )
+            ax.plot(
+                sorted_n_samples,
+                sorted_mean,
+                marker="x",
+                linestyle="-",
+                color=method_colors[method],
+                label=f"{method_names[method]} + std band",
+            )
 
-        ax.set_ylabel(measure)
-        ax.set_xticks(np.arange(len(names)))
-        ax.set_xticklabels(names, rotation=90)
-        ax.grid(True, which="both", ls=":")
-        ax.legend()
+            # if method == methods[-1]:
+            #     ax.plot(
+            #         [10, 50000],
+            #         [0.6 / (2**4)] * 2,
+            #         linestyle="--",
+            #         color="black",
+            #         label="True SPSF violation",
+            #     )
+
+            ax.set_title(f"{scenario}")
+            ax.set_xscale("log")
+            ax.set_xlabel("Number of Samples [Log Scale]")
+            ax.set_ylabel(f"{measure}")
+            ax.grid(True, which="both", ls=":")
+            ax.legend()
 
 plt.tight_layout()
-output_path = "multirun_images/" + str(date.today()) + "_real_data.png"
+output_path = "multirun_images/" + str(date.today()) + "_real_data_line.png"
 plt.savefig(output_path)
 
 plt.show()
