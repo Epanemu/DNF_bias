94b89840202019b91b00a285231aebb488461a51
Jiri Nemecek <contact@nemecekjiri.cz>
2025-01-12 22:00:01 +0100

    Unifiy experiments

--- experiment_classif.py
+++ experiment_classif.py
@@ -6,61 +6,51 @@ import sys
 
 import hydra
 import numpy as np
-from folktables import ACSDataSource
+import pandas as pd
 from omegaconf import DictConfig
 
-from binarizer import Binarizer
-from data_handler import DataHandler
 from dnf_fair_classifier import DNFFairClassifier
+from gerryfair.auditor import Auditor
+from scenarios.folktables_scenarios import load_classif_scenario
+from spsf_mio import SPSF
+from utils import eval_fpsf, eval_spsf
 
 githash = ""
 
 
 @hydra.main(version_base="1.3", config_path="conf", config_name="config")
 def run_experiment(cfg: DictConfig):
-    if cfg.scenario == "ACSIncome":
-        from folktables import ACSIncome as Dataset
-    elif cfg.scenario == "ACSPublicCoverage":
-        from folktables import ACSPublicCoverage as Dataset
-    elif cfg.scenario == "ACSMobility":
-        from folktables import ACSMobility as Dataset
-    elif cfg.scenario == "ACSEmployment":
-        from folktables import ACSEmployment as Dataset
-    elif cfg.scenario == "ACSTravelTime":
-        from folktables import ACSTravelTime as Dataset
-    else:
-        raise ValueError(f'Scenario "{cfg.scenario}" does not exist.')
-
-    # TODO make the configuration parameterized
-    data_source = ACSDataSource(survey_year="2018", horizon="1-Year", survey="person")
-    data = data_source.get_data(states=["CA"], download=True)
-    input_data, target_data, _ = Dataset.df_to_pandas(data)
-
-    # DROP COLS WITH TOO MANY OPTIONS
-    to_drop = []
-    for col in input_data.columns:
-        vals = input_data[col].unique().shape[0]
-        if vals > 5 or vals <= 1:
-            to_drop.append(col)
-    input_data.drop(columns=to_drop, inplace=True)
-
-    np.random.seed(cfg.seed)
-    n = input_data.shape[0]
-    samples = np.random.choice(n, size=min(cfg.n_samples, n), replace=False)
-
-    input_data = input_data.iloc[samples]
-    target_data = target_data[target_data.columns[0]].iloc[samples]
-    dhandler = DataHandler.from_data(
-        input_data, target_data, categ_map={c: [] for c in input_data.columns}
+    binarizer, X_orig, y_orig, binarizer_protected, X_prot_orig = load_classif_scenario(
+        cfg.scenario, cfg.seed, cfg.n_samples
     )
-    binarizer = Binarizer(dhandler, target_positive_vals=[True])
 
-    X = binarizer.encode(input_data, include_negations=True)
-    y = binarizer.encode_y(target_data)
+    X = binarizer.encode(X_orig, include_negations=True)
+    X_prot = binarizer_protected.encode(X_prot_orig, include_negations=True)
+    y = binarizer.encode_y(y_orig)
 
     dnf = DNFFairClassifier(gamma=0.01)
 
-    result = dnf.find_dnf(X, y, n_terms=5, time_limit=300, verbose=True)
+    result = dnf.find_dnf(X, X_prot, y, n_terms=5, time_limit=900, verbose=True)
+    y_hat_train = np.zeros_like(y, dtype=bool)
+    for term in result:
+        y_term = np.ones_like(y, dtype=bool)
+        for conj in term:
+            y_term &= X[:, conj]
+        y_hat_train |= y_term
+
+    # Evaluate using gerryfair !and! SPSF_mio
+    auditor = Auditor(pd.DataFrame(X_prot), pd.Series(y), "FP")
+    gerrygroup_train, oracle = auditor.audit(
+        pd.Series(y_hat_train), with_group_def=True
+    )
+
+    spsf = SPSF()
+    mask = y == 0
+    group_rule = spsf.find_rule(X_prot[mask], y_hat_train[mask])
+
+    miogroup_train = np.ones_like(y, dtype=bool)
+    for feat_i in group_rule:
+        miogroup_train &= X_prot[:, feat_i]
 
     # Get the current working directory, which Hydra sets for each run
     run_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
@@ -79,6 +69,20 @@ def run_experiment(cfg: DictConfig):
             )
             out_file.write(f"Number of cuts: {dnf.n_cuts} \n")
             out_file.write(f"Number of callbacks: {dnf.n_callbacks} \n")
+            out_file.write(
+                f"Time in callbacks proportion: {dnf.callback_time_proportion} \n"
+            )
+            out_file.write(f"MIO group: {group_rule} \n")
+            out_file.write(f"MIO SPSF: {eval_spsf(y_hat_train, miogroup_train)} \n")
+            out_file.write(f"MIO FPSF: {eval_fpsf(y, y_hat_train, miogroup_train)} \n")
+            out_file.write(f"Gerry oracle b0 coef:\n {oracle.b0.coef_} \n")
+            out_file.write(f"Gerry oracle b0 intercept: {oracle.b0.intercept_} \n")
+            out_file.write(f"Gerry oracle b1 coef:\n {oracle.b1.coef_} \n")
+            out_file.write(f"Gerry oracle b1 intercept: {oracle.b1.intercept_} \n")
+            out_file.write(f"Gerry SPSF: {eval_spsf(y_hat_train, gerrygroup_train)} \n")
+            out_file.write(
+                f"Gerry FPSF: {eval_fpsf(y, y_hat_train, gerrygroup_train)} \n"
+            )
         else:
             out_file.write("Error\n")
 
--- experiment_classif_gerry.py
+++ experiment_classif_gerry.py
@@ -1,18 +1,18 @@
 # python experiment.py -m
 
 import os
+import pickle
 import subprocess
 import sys
 
 import hydra
 import numpy as np
 import pandas as pd
-from folktables import ACSDataSource
 from omegaconf import DictConfig
 
-from binarizer import Binarizer
-from data_handler import DataHandler
 from gerryfair.model import Auditor, Model
+from scenarios.folktables_scenarios import load_classif_scenario
+from spsf_mio import SPSF
 from utils import eval_fpsf, eval_spsf
 
 githash = ""
@@ -20,67 +20,34 @@ githash = ""
 
 @hydra.main(version_base="1.3", config_path="conf", config_name="config")
 def run_experiment(cfg: DictConfig):
-    if cfg.scenario == "ACSIncome":
-        from folktables import ACSIncome as Dataset
-    elif cfg.scenario == "ACSPublicCoverage":
-        from folktables import ACSPublicCoverage as Dataset
-    elif cfg.scenario == "ACSMobility":
-        from folktables import ACSMobility as Dataset
-    elif cfg.scenario == "ACSEmployment":
-        from folktables import ACSEmployment as Dataset
-    elif cfg.scenario == "ACSTravelTime":
-        from folktables import ACSTravelTime as Dataset
-    else:
-        raise ValueError(f'Scenario "{cfg.scenario}" does not exist.')
-
-    # TODO make the configuration parameterized
-    data_source = ACSDataSource(survey_year="2018", horizon="1-Year", survey="person")
-    data = data_source.get_data(states=["CA"], download=True)
-    input_data, target_data, _ = Dataset.df_to_pandas(data)
-
-    # DROP COLS WITH TOO MANY OPTIONS
-    to_drop = []
-    for col in input_data.columns:
-        vals = input_data[col].unique().shape[0]
-        if vals > 5 or vals <= 1:
-            to_drop.append(col)
-    input_data.drop(columns=to_drop, inplace=True)
-
-    np.random.seed(cfg.seed)
-    n = input_data.shape[0]
-    samples = np.random.choice(n, size=min(cfg.n_samples, n), replace=False)
-
-    target_data = target_data[target_data.columns[0]]
-
-    train_data = input_data.iloc[samples]
-    train_y_data = target_data.iloc[samples]
-    dhandler = DataHandler.from_data(
-        input_data, target_data, categ_map={c: [] for c in input_data.columns}
+    binarizer, X_orig, y_orig, binarizer_protected, X_prot_orig = load_classif_scenario(
+        cfg.scenario, cfg.seed, cfg.n_samples
     )
-    binarizer = Binarizer(dhandler, target_positive_vals=[True])
 
-    X_test = pd.DataFrame(binarizer.encode(input_data, include_negations=False))
-    y_test = pd.Series(binarizer.encode_y(target_data))
-    X = pd.DataFrame(binarizer.encode(train_data, include_negations=False))
-    y = pd.Series(binarizer.encode_y(train_y_data))
+    X = binarizer.encode(X_orig, include_negations=False)
+    X_prot = binarizer_protected.encode(X_prot_orig, include_negations=False)
+    y = binarizer.encode_y(y_orig)
+
+    dfX = pd.DataFrame(X)
+    dfX_prot = pd.DataFrame(X_prot)
+    dfy = pd.Series(y)
 
     fair_model = Model(printflag=True, gamma=0.01, fairness_def="FP")
     fair_model.set_options(max_iters=30)
-    # train_data.index = np.arange(train_data.index.shape[0])
-    # train_y_data.index = np.arange(train_data.index.shape[0])
-    # fair_model.train(train_data, train_data, train_y_data)
-    fair_model.train(X, X, y)
+    fair_model.train(dfX, dfX_prot, dfy)
 
-    # y_hat_train = fair_model.predict(train_data)
-    # auditor = Auditor(train_data, train_y_data, "FP")
-    # violated_group_train, _ = auditor.audit(y_hat_train)
-    y_hat_train = fair_model.predict(X)
-    auditor = Auditor(X, y, "FP")
-    violated_group_train, _ = auditor.audit(y_hat_train)
+    dfy_hat_train = fair_model.predict(X)
+    auditor = Auditor(dfX_prot, dfy, "FP")
+    gerrygroup_train, oracle = auditor.audit(dfy_hat_train, with_group_def=True)
 
-    y_hat = fair_model.predict(X_test)
-    auditor = Auditor(X_test, y_test, "FP")
-    violated_group, _ = auditor.audit(y_hat)
+    y_hat_train = dfy_hat_train.values
+    spsf = SPSF()
+    mask = y == 0
+    group_rule = spsf.find_rule(X_prot[mask], y_hat_train[mask])
+
+    miogroup_train = np.ones_like(y, dtype=bool)
+    for feat_i in group_rule:
+        miogroup_train &= X_prot[:, feat_i]
 
     # Get the current working directory, which Hydra sets for each run
     run_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
@@ -92,26 +59,19 @@ def run_experiment(cfg: DictConfig):
         out_file.write(f"\nGit hash: {githash}\n\n")
 
         out_file.write("RESULT\n")
-        out_file.write(
-            f"Train Accuracy: {np.mean(np.array(y_hat_train,dtype=bool) == y.values)} \n"
-        )
-        out_file.write(
-            f"Train SPSF: {eval_spsf(np.array(y_hat_train,dtype=bool), np.array(violated_group_train,dtype=bool))} \n"
-        )
-        out_file.write(
-            f"Train FPSF: {eval_fpsf(y.values, np.array(y_hat_train,dtype=bool), np.array(violated_group_train,dtype=bool))} \n"
-        )
-        out_file.write(
-            f"Test Accuracy: {np.mean(np.array(y_hat,dtype=bool) == y_test.values)} \n"
-        )
-        out_file.write(
-            f"Test SPSF: {eval_spsf(np.array(y_hat,dtype=bool), np.array(violated_group,dtype=bool))} \n"
-        )
-        out_file.write(
-            f"Test FPSF: {eval_fpsf(y_test.values, np.array(y_hat,dtype=bool), np.array(violated_group,dtype=bool))} \n"
-        )
+        out_file.write(f"MIO group: {group_rule} \n")
+        out_file.write(f"MIO SPSF: {eval_spsf(y_hat_train, miogroup_train)} \n")
+        out_file.write(f"MIO FPSF: {eval_fpsf(y, y_hat_train, miogroup_train)} \n")
+        out_file.write(f"Gerry oracle b0 coef:\n {oracle.b0.coef_} \n")
+        out_file.write(f"Gerry oracle b0 intercept: {oracle.b0.intercept_} \n")
+        out_file.write(f"Gerry oracle b1 coef:\n {oracle.b1.coef_} \n")
+        out_file.write(f"Gerry oracle b1 intercept: {oracle.b1.intercept_} \n")
+        out_file.write(f"Gerry SPSF: {eval_spsf(y_hat_train, gerrygroup_train)} \n")
+        out_file.write(f"Gerry FPSF: {eval_fpsf(y, y_hat_train, gerrygroup_train)} \n")
 
     print(f"Result saved to {os.path.join(run_dir, 'output.txt')}")
+    with open(os.path.join(run_dir, "gerrymodel.pickle"), "wb") as f:
+        pickle.dump(fair_model, f)
 
 
 if __name__ == "__main__":
