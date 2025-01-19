3c288a516e4d938a6ee551e9dcb41588ea799df8
Jiri Nemecek <contact@nemecekjiri.cz>
2025-01-19 16:43:17 +0100

    Setup for 10k alpha + MSE

--- experiment_classif.py
+++ experiment_classif.py
@@ -67,7 +67,7 @@ def run_experiment(cfg: DictConfig):
     elif cfg.model == "NN":
         # alpha = 1/gamma
         NN = NNFairClassifier(
-            X_enc.shape[1], [500, 200, 50, 10], gamma=0.01, alpha=1000
+            X_enc.shape[1], [500, 200, 50, 10], gamma=0.01, alpha=10000
         )
         np.random.seed(cfg.seed)
         eval_idx = np.random.choice(n_samples, n_samples // 10, replace=False)
